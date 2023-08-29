9c3a3143120c1c49502be2790c267d1157aaf9aa
Moises <moyses1234567890@gmail.com>
2023-08-29 15:36:38 -0600

    Se realizo el routing en dashbard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -93,6 +93,14 @@ const routes: Routes = [
             path: 'add-your-location',
             loadChildren: () => import('./profile/add-your-location/add-your-location.module').then( m => m.AddYourLocationPageModule)
           },
+          {
+            path: 'chatbot',
+            loadChildren: () => import('./pages/chatbot/chatbot.module').then( m => m.ChatbotPageModule)
+          },
+          {
+            path: 'notifications',
+            loadChildren: () => import('./pages/notifications/notifications.module').then( m => m.NotificationsPageModule)
+          },
           // {
           //   path: 'radio',
           //   loadChildren: () => import('./radio/radio-page.module').then((m) => m.RadioPageModule),
